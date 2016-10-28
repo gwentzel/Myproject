2f7572c7af689ca2e4ded99c2671307576e606e6
gwentzel <ggwentzel@hotmail.com>
2016-10-27 22:13:19 -0400

    Rendering seat chart now

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,4 +1,4 @@
-Basic setup:
+/* Basic setup:   */
 
 	$(document).ready(function() {
 
@@ -15,9 +15,21 @@ Basic setup:
 				a: {
 					price   : 12.00,
 					classes : 'front-seat' //your custom CSS class
-				}
+				},
+				v: {
+					classes : 'available' //your custom CSS class
+				},
+				e: {
+						classes : 'sold' //your custom CSS class
+				}	},
 
-			},
+		/*	legend : {
+				 node : $('#my-legend-container'),
+				 items: [
+					[v, 'available','open'],
+					[e,'unavailable','sold'] ]
+			},  */
+
 			click: function () {
 				if (this.status() == 'available') {
 					//do some stuff, i.e. add to the cart
@@ -31,5 +43,24 @@ Basic setup:
 				} else {
 					return this.style();
 				}
-			}
+
+}
+
+		});
+
+		//Make all available 'c' seats unavailable
+		sc.find('c.available').status('unavailable');
+
+		/*
+		Get seats with ids 2_6, 1_7 (more on ids later on),
+		put them in a jQuery set and change some css
+		*/
+		sc.get(['2_6', '1_7']).node().css({
+			color: '#ffcfcf'
+		});
+
+
+
+		console.log('Seat 1_2 costs ' + sc.get('1_2').data().price + ' and is currently ' + sc.status('1_2'));
+
 		});
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,25 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express theatre' });
 });
+
+ /* handler fired in seat context(this)
+click : function(){
+  if (this.status() =="available'"){
+    var price = this.data().price,
+    category = this.data().category;
+    this.node().css({
+      'font-size': '25px'
+    });
+    return 'selected';
+  }
+}
+*/
+
+
+
+
+
 
 module.exports = router;
